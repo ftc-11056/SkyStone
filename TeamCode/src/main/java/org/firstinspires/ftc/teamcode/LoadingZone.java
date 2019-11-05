71c961814104c7ba9808b49f86df001c91f82afa
TheYbotS <clil.shoham@gmail.com>
2019-11-05 21:10:50 +0200

    basic auto and basic teleop workingggg

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LoadingZone.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LoadingZone.java
@@ -3,6 +3,8 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+
 
 @Autonomous(name = "LoadingZone", group = "teamcode")
 public class LoadingZone extends basicAuto {
@@ -20,7 +22,6 @@ public class LoadingZone extends basicAuto {
         //   RightServo.setPosition(0.5);
 
         waitForStart();
-
         telemetry.addData("Angles", MyDriveTrain.getAngle());
         telemetry.update();
         telemetry.addData("Mikum:", Mikum);
@@ -30,7 +31,7 @@ public class LoadingZone extends basicAuto {
         LB.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
         RB.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
 
-        MyDriveTrain.encoderDrive(1,-30,30,30,-30 ,1);
+        MyDriveTrain.encoderDrive(0.8,-30,30,30,-30 ,1);
         Mikum = MyVuforiaStone.ConceptVuforiaSkyStoneNavigationWebcam();
 
         LF.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
@@ -42,98 +43,156 @@ public class LoadingZone extends basicAuto {
             telemetry.addLine("you're on the right");
             telemetry.update();
             MyDriveTrain.encoderDrive(1,85,85,85,85,2);
-            sleep(500);
-            MyDriveTrain.encoderDrive(1,33,33,-33,-33,2);
+//            sleep(500);
+            MyDriveTrain.encoderDrive(1,36,36,-36,-36,2);
             sleep(500);
             MyIntake.maxIntake();
-            MyDriveTrain.encoderDrive(0.7,-75,-75,-75,-75,2);
-            sleep(500);
+            MyDriveTrain.encoderDrive(0.7,-80,-80,-80,-80,2);
+            if (cubeIn.getDistance(DistanceUnit.MM) > 60){
+                MyDriveTrain.encoderDrive(0.7,-20,-20,-20,-20,2);
+                telemetry.addData("CubeDistans:",cubeIn.getDistance(DistanceUnit.MM));
+                telemetry.update();
+            }
+//            sleep(500);
             MyIntake.ShutDown();
             MyDriveTrain.encoderDrive(1,60,60,60,60,2);
-            sleep(500);
-            MyDriveTrain.encoderDrive(1,-35,-35,35,35,2);
-            sleep(500);
-            MyDriveTrain.encoderDrive(1,60,60,60,60,2);
-            sleep(500);
+//            sleep(500);
+            MyDriveTrain.encoderDrive(1,-37,-37,37,37,2);
+//            sleep(500);
+            MyDriveTrain.encoderDrive(1,70,70,70,70,2);
+//            sleep(500);
 
 
         }
         else if (Mikum<-2) {
             telemetry.addLine("you're on the left");
             telemetry.update();
-            MyDriveTrain.encoderDrive(0.7,23,23,23,23,2);
-            sleep(500);
-            MyDriveTrain.encoderDrive(0.7,-45,45,45,-45,1);
-            sleep(500);
+            MyDriveTrain.encoderDrive(1,43,43,43,43,2);
+//            sleep(500);
+            MyDriveTrain.encoderDrive(0.8,-90,90,90,-90,1);
+//            sleep(500);
             MyIntake.maxIntake();
-            MyDriveTrain.encoderDrive(0.7,-25,-25,-25,-25,2);
-            sleep(500);
+            MyDriveTrain.encoderDrive(1,-25,-25,-25,-25,2);
+//            sleep(500);
+            if (cubeIn.getDistance(DistanceUnit.MM) > 60){
+                MyDriveTrain.encoderDrive(0.7,-20,-20,-20,-20,2);
+                telemetry.addData("CubeDistans:",cubeIn.getDistance(DistanceUnit.MM));
+                telemetry.update();
+            }
             MyIntake.ShutDown();
-            MyDriveTrain.encoderDrive(0.7,42,-42,-42,42,1);
-            sleep(500);
-            MyDriveTrain.encoderDrive(0.7,50,50,50,50,2);
-            sleep(500);
+            MyDriveTrain.encoderDrive(0.8,65,-65,-65,65,1);
+//            sleep(500);
+            MyDriveTrain.encoderDrive(1,120,120,120,120,2);
+//            sleep(500);
 
         }
         else {
             telemetry.addLine("You are on the center!");
             telemetry.update();
-            MyDriveTrain.encoderDrive(0.7,28,28,28,28,2);
-            sleep(500);
-            MyDriveTrain.encoderDrive(0.7,-45,45,45,-45,1);
-            sleep(500);
+            MyDriveTrain.encoderDrive(1,50,50,50,50,2);
+//            sleep(500);
+            MyDriveTrain.encoderDrive(0.8,-90,90,90,-90,1);
+//            sleep(500);
             MyIntake.maxIntake();
-            MyDriveTrain.encoderDrive(0.7,-25,-25,-25,-25,2);
+            MyDriveTrain.encoderDrive(1,-25,-25,-25,-25,2);
             sleep(500);
+            if (cubeIn.getDistance(DistanceUnit.MM) > 60){
+                MyDriveTrain.encoderDrive(0.7,-20,-20,-20,-20,2);
+                telemetry.addData("CubeDistans:",cubeIn.getDistance(DistanceUnit.MM));
+                telemetry.update();
+            }
             MyIntake.ShutDown();
-            MyDriveTrain.encoderDrive(0.7,40,-40,-40,40,1);
-            sleep(500);
-            MyDriveTrain.encoderDrive(0.7,50,50,50,50,2);
-            sleep(500);
+            MyDriveTrain.encoderDrive(0.8,65,-65,-65,65,1);
+//            sleep(500);
+            MyDriveTrain.encoderDrive(1,120,120,120,120,2);
+//            sleep(500);
         }
 
-        MyDriveTrain.encoderDrive(0.6,60,60,-60,-60,2);
-        sleep(500);
+        MyDriveTrain.encoderDrive(0.8,60,60,-60,-60,2);
+//        sleep(500);
         MyIntake.maxOuttake();
         sleep(1000);
         MyIntake.ShutDown();
-        MyDriveTrain.encoderDrive(0.6,-55,-55,55,55,2);
-        sleep(500);
+        MyDriveTrain.encoderDrive(0.8,-55,-55,55,55,2);
+//        sleep(500);
 
         if (Mikum>2) {
 //            telemetry.addLine("you're on the right");
 //            telemetry.update();
-            MyDriveTrain.encoderDrive(0.7,-100,-100,-100,-100,2);
-            sleep(500);
-            MyDriveTrain.encoderDrive(0.7,-80,80,80,-80,1);
+            MyDriveTrain.encoderDrive(1,-117,-117,-117,-117,2);
+//            sleep(500);
+            MyDriveTrain.encoderDrive(1,-65,65,65,-65,1);
             sleep(500);
             MyIntake.maxIntake();
-            MyDriveTrain.encoderDrive(0.7,-25,-25,-25,-25,2);
+            MyDriveTrain.encoderDrive(1,-25,-25,-25,-25,2);
             sleep(500);
+            if (cubeIn.getDistance(DistanceUnit.MM)>60){
+                MyDriveTrain.encoderDrive(1,-20,-20,-20,-20,2);
+                telemetry.addData("CubeDistans:",cubeIn.getDistance(DistanceUnit.MM));
+                telemetry.update();
+            }
+//            sleep(500);
             MyIntake.ShutDown();
-            MyDriveTrain.encoderDrive(0.7,75,-75,-75,75,1);
-            sleep(500);
-            MyDriveTrain.encoderDrive(0.7,120,120,120,120,2);
-            sleep(500);
+            MyDriveTrain.encoderDrive(1,60,-60,-60,60,1);
+//            sleep(500);
+            MyDriveTrain.encoderDrive(1,150,150,150,150,2);
+//            sleep(500);
         }
         else if (Mikum<-2) {
 //            telemetry.addLine("you're on the left");
 //            telemetry.update();
+            MyDriveTrain.encoderDrive(1,-140,-140,-140,-140,2);
+//            sleep(500);
+            MyDriveTrain.encoderDrive(0.8,-70,70,70,-70,1);
+//            sleep(500);
+            MyIntake.maxIntake();
+            MyDriveTrain.encoderDrive(1,-25,-25,-25,-25,2);
+            sleep(500);
+            if (cubeIn.getDistance(DistanceUnit.MM)>60){
+                MyDriveTrain.encoderDrive(0.7,-20,-20,-20,-20,2);
+                telemetry.addData("CubeDistans:",cubeIn.getDistance(DistanceUnit.MM));
+                telemetry.update();
+            }
+//            sleep(500);
+            MyIntake.ShutDown();
+            MyDriveTrain.encoderDrive(0.8,60,-60,-60,60,1);
+//            sleep(500);
+            MyDriveTrain.encoderDrive(1,180,180,180,180,2);
+//            sleep(500);
 
 
         }
         else {
 //            telemetry.addLine("You are on the center!");
 //            telemetry.update();
+            MyDriveTrain.encoderDrive(1,-140,-140,-140,-140,2);
+//            sleep(500);
+            MyDriveTrain.encoderDrive(0.8,-70,70,70,-70,1);
+//            sleep(500);
+            MyIntake.maxIntake();
+            MyDriveTrain.encoderDrive(1,-25,-25,-25,-25,2);
+            sleep(500);
+            if (cubeIn.getDistance(DistanceUnit.MM)>60){
+                MyDriveTrain.encoderDrive(0.7,-20,-20,-20,-20,2);
+                telemetry.addData("CubeDistans:",cubeIn.getDistance(DistanceUnit.MM));
+                telemetry.update();
+            }
+//            sleep(500);
+            MyIntake.ShutDown();
+            MyDriveTrain.encoderDrive(0.8,60,-60,-60,60,1);
+//            sleep(500);
+            MyDriveTrain.encoderDrive(1,180,180,180,180,2);
+//            sleep(500);
 
         }
 
-        MyDriveTrain.encoderDrive(0.6,60,60,-60,-60,2);
-        sleep(500);
+        MyDriveTrain.encoderDrive(0.8,60,60,-60,-60,2);
+//        sleep(500);
         MyIntake.maxOuttake();
         sleep(1000);
         MyIntake.ShutDown();
-        MyDriveTrain.encoderDrive(0.7,20,-20,-20,20,1);
+        MyDriveTrain.encoderDrive(1,10,10,10,10,1);
+        MyDriveTrain.encoderDrive(1,40,-40,-40,40,1);
 
 
 
