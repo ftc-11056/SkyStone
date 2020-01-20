6bbdcaf1e53be856f35df1503ec7b580992da5b0
TheYbotS <clil.shoham@gmail.com>
2020-01-20 15:37:46 +0200

    new foundation

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomuses/FoundationPlacementBlue.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomuses/FoundationPlacementBlue.java
@@ -19,30 +19,24 @@ public class FoundationPlacementBlue extends basicAuto {
     public void runOpMode() throws InterruptedException {
         super.runOpMode();
         waitForStart();
-       // programRunTime.reset();
-        MyDriveTrain.encoderDrive(0.5,-30,30,30,-30,2);
-        MyDriveTrain.encoderDrive(0.5,-20,-20,-20,-20,2);
-        MyDriveTrain.RotateP(90,0.7,10,0.0108);
-        MyDriveTrain.encoderDrive(0.5,40,40,40,40,2);
-        MyDriveTrain.Rotate(90,0.1,10);
-        LeftServo.setPosition(0.15);
-        RightServo.setPosition(0.2);
-        MyDriveTrain.encoderDrive(0.1,10,10,10,10,2);
-        LeftServo.setPosition(LeftServoDown);
-        RightServo.setPosition(RightServoDown);
-        sleep(500);
-        MyDriveTrain.encoderDrive(1,-90,-90,-90,-90,2);
-        MyDriveTrain.Rotate(180,0.5,10);
-        MyDriveTrain.encoderDrive(1,40,40,40,40,2);
-        LeftServo.setPosition(LeftServoUp);
-        RightServo.setPosition(RightServoUp);
-        MyDriveTrain.Rotate(180,0.4,10);
-        MyDriveTrain.encoderDrive(0.3,30,-30,-30,30,2);
-        ParkingMot.setPosition(ParkingMotIn);
-        sleep(500);
-        MyDriveTrain.encoderDrive(0.5,-43,-43, -43,-43,1);
+        while (Touch_Foundation.getState()) {
+            RB.setPower(-0.4);
+            LB.setPower(-0.4);
+            RF.setPower(-0.4);
+            LF.setPower(-0.4);
+            LeftServo.setPosition(LeftServoDown);
+            RightServo.setPosition(RightServoDown);
+            MyDriveTrain.encoderDrive(1, 110, 110, 110, 110, 2);
+            MyDriveTrain.encoderDrive(0.3, -140, -140, 140, 140, 2);
+            LeftServo.setPosition(LeftServoUp);
+            RightServo.setPosition(RightServoUp);
+            ParkingMot.setPosition(ParkingMotIn);
+            sleep(500);
+            MyDriveTrain.encoderDrive(0.5, -10, -10, -10, -10, 1);
+            MyDriveTrain.encoderDrive(0.5, 53, 53, 53, 53, 1);
 
+
+        }
 
     }
-
 }
