ddab8f44e91c92997a488fe6a7130c1fb78834a2
the-y-bot-2019 <theybot11056@gmail.com>
2020-01-29 13:55:29 +0200

    to merge

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CheckList.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CheckList.java
@@ -1,13 +1,17 @@
-package org.firstinspires.ftc.teamcode.Autonomuses;
+package org.firstinspires.ftc.teamcode;
+
+import android.support.v4.app.ServiceCompat;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 import org.firstinspires.ftc.teamcode.basicAuto;
+import org.firstinspires.ftc.teamcode.basicAutoCustumade;
 
 
 @Autonomous(name = "CheckList", group = "teamcode")
-public class CheckList extends basicAuto {
+public class CheckList extends basicAutoCustumade {
 
     /* Declare OpMode members. */
 
@@ -18,8 +22,6 @@ public class CheckList extends basicAuto {
     @Override
     public void runOpMode() throws InterruptedException {
         super.runOpMode();
-//  LeftServo.setPosition(0.4);
-//   RightServo.setPosition(0.5);
 
         waitForStart();
         runtime.reset();
@@ -71,24 +73,48 @@ public class CheckList extends basicAuto {
 
         MyDriveTrain.Rotate(90,1,10);
 
-        MyIntake.maxIntake();
         MyElevator.ElevateWithEncoder(-500, 0.3, 0.5);
+
         sleep(1000);
-        MyIntake.ShutDown();
-        Arm.setPosition(1);
+        Capstone.setPosition(CapstoneDown);
+        Capstone.setPosition(CapstoneUp);
         sleep(1000);
+
         MyElevator.ElevateWithEncoder(0, 0.1, 0.003);
+
+        MyIntake.maxIntake();
+        sleep(1000);
+        MyIntake.maxOuttake();
+
+        MyDriveTrain.SetPower(0.8,0.8,0.8,0.8);
+        sleep(1000);
+        MyDriveTrain.SetPower(0,0,0,0);
+        telemetry.addData("Odometry Y", MyOdometry.getPosition().getY());
+        telemetry.update();
+        sleep(4000);
+
+        MyDriveTrain.SetPower(-0.8,0.8,0.8,-0.8);
+        sleep(1000);
+        MyDriveTrain.SetPower(0,0,0,0);
+        telemetry.addData("Odometry X", MyOdometry.getPosition().getX());
+        telemetry.update();
+
+        sleep(4000);
+
+        Arm.setPosition(ArmOpen);
+        Arm.setPosition(ArmClose);
 
         LeftServo.setPosition(LeftServoDown);
         RightServo.setPosition(RightServoDown);
         sleep(1000);
         LeftServo.setPosition(LeftServoUp);
         RightServo.setPosition(RightServoUp);
+
         sleep(1000);
 
-        Output.setPosition(OutputDown);
+        Output.setPosition(OutputClose);
         sleep(1000);
-        Output.setPosition(OutputUp);
+        Output.setPosition(OutputOpen);
         sleep(1000);
 
         ParkingMot.setPosition(ParkingMotIn);
@@ -96,7 +122,14 @@ public class CheckList extends basicAuto {
         ParkingMot.setPosition(ParkingMotOut);
         sleep(1000);
 
+        telemetry.addData("down Magnet", downMagnetElevator.getState());
+        telemetry.addData("CubeIn", cubeIn.getDistance(DistanceUnit.MM));
+        telemetry.update();
+        sleep(3000);
 
+        telemetry.addData("left touch", LeftTouch.getState());
+        telemetry.addData("right touch", RightTouch.getState());
+        telemetry.update();
+        sleep(2000);
     }
-
 }
